4dcfbdc57ac11ad3e33ccd093682470a429808c2
thtrina-github <53204624+thtrina@users.noreply.github.com>
2026-07-21 14:11:18 -0500

    Refactor ComparisonResult class with new fields
    Refactored ComparisonResult class to include additional fields for 'last', 'first', 'dev', and 'prod'.

--- ComparisonResult.java
+++ ComparisonResult.java
@@ -1,3 +1,29 @@
+public class ComparisonResult {
+
+    String type;
+    String id;
+    String column;
+    String oldValue;
+    String newValue;
+    String message;
+
+    public ComparisonResult(
+            String type,
+            String id,
+            String column,
+            String oldValue,
+            String newValue,
+            String message) {
+
+        this.type = type;
+        this.id = id;
+        this.column = column;
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+        this.message = message;
+    }
+}
+-----------------------------new to look for duplicate rows
 public class ComparisonResult{
  public String type,id,last,first,column,dev,prod;
  public ComparisonResult(String t,String i,String l,String f,String c,String d,String p){
